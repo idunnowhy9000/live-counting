071c7a8e430b3405f5c5ef74c2921f188d97ee99
idunnowhy9000 <idunnowhy9000@gmail.com>
2015-07-04 16:37:57 +0700

    ayy lmao

---
+++ app.js
@@ -0,0 +1,76 @@
+var express = require('express');
+//var socketio = require('socket.io');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
+var path = require('path');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var router = require('./routers/router');
+
+var app = module.exports = express();
+
+// view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// middlewares
+app.use(compression());
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(favicon(path.join(__dirname, 'views/favicon.ico')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// serve static
+app.use(express.static(path.join(__dirname, 'views')));
+app.use('/', router);
+
+// passport config
+var User = require('./models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/livecounting');
+
+// api router
+app.use('/api', require('./routes/api'));
+
+// catch 404
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
