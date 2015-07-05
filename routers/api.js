cf73a2920308c9ffb2fdeef1def4cf780389aee8
idunnowhy9000 <idunnowhy9000@gmail.com>
2015-07-05 07:08:16 +0700

    ayy lmao

--- routers/api.js
+++ routers/api.js
@@ -4,8 +4,33 @@ var User = require('../models/User');
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    
+router.get('/user/:name', function (req, res) {
+    User.find({ username: req.name }).exec(function (err, user) {
+        if (err) {
+            res.send({ error: 'User does not exist' });
+        } else {
+            res.send(user);
+        }
+    });
+});
+
+router.post('/register', function (req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+        if (err) {
+            res.send({ error: 'Error while registering' });
+        } else {
+            res.send({});
+        }
+    });
+});
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.send({});
+});
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.send({});
 });
 
 module.exports = router;
